2f6c80e590ebcebca7c0a74533ffc6caf8b8b19d
RoxBlox3 <roxblox33@proton.me>
2025-01-26 17:56:39 +0100

    Put more folders

--- Learn_Python/Ch4_Scope/L2_Global_Scope/L2_Global_Scope.py
+++ Learn_Python/Ch4_Scope/L2_Global_Scope/L2_Global_Scope.py
@@ -16,4 +16,3 @@ def calculate_primary_stats(armor_bonus, modifier):
 print(f"Character has {calculate_health(10)} max health.")
 
 print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
-
