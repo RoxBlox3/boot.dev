a72bb46df937ea7c96d082b467e8cf53b70e7458
RoxBlox3 <RoxBlox33@proton.me>
2025-02-07 13:41:17 +0100

    finished L13 bitwise & operator

--- Learn_Python/Ch6_Computing/L13_Bitwise&Operator/main.py
+++ Learn_Python/Ch6_Computing/L13_Bitwise&Operator/main.py
@@ -5,17 +5,17 @@ can_edit_guild = 0b0001
 
 
 def get_create_bits(user_permissions):
-    pass
-
+    user_create_guild_permission = user_permissions & can_create_guild
+    return user_create_guild_permission
 
 def get_review_bits(user_permissions):
-    pass
-
+    user_review_guild_permission = user_permissions & can_review_guild
+    return user_review_guild_permission
 
 def get_delete_bits(user_permissions):
-    pass
-
+    user_delete_guild_permission = user_permissions & can_delete_guild
+    return user_delete_guild_permission
 
 def get_edit_bits(user_permissions):
-    pass
-
+    user_edit_guild_permission = user_permissions & can_edit_guild
+    return user_edit_guild_permission
