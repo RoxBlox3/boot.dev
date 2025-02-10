2649f6babcdf703fba0bbb96223d525315c302af
RoxBlox3 <RoxBlox33@proton.me>
2025-02-10 13:50:37 +0100

    Finished Ch6 Computing

--- Learn_Python/Ch6_Computing/L14_Bitwise | operator/main.py
+++ Learn_Python/Ch6_Computing/L14_Bitwise | operator/main.py
@@ -1,4 +1,4 @@
 def calculate_guild_perms(glorfindel, galadriel, elendil, elrond):
-    permition = glorfindel | galadriel | elendil | elrond
-    return permition
+    permission = glorfindel | galadriel | elendil | elrond
+    return permission
 
