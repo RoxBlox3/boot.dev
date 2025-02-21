c09fdd0146e694ed62fa7db328ebcbf3789fcbb2
RoxBlox3 <RoxBlox33@proton.me>
2025-02-21 17:06:13 +0100

    Finishedd Ch8 C4

--- Learn_Python/Ch8_Loops/C4_Random_Events/main.py
+++ Learn_Python/Ch8_Loops/C4_Random_Events/main.py
@@ -1,5 +1,14 @@
 def is_prime(number):
     if number > 1:
-        for i in range(2, number+1):
+        for i in range(2, (number+2)//2):
             result = number % i
             print(f"{result} {i}")
+            if result == 0:
+                print(f"{number} is not a prime number.")
+                return False
+                break
+        else:
+            print(f"Return loop number {number} result") 
+            return True
+    else:
+        return False
