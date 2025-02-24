ffe72230c72581054d548f2be85fe78660f9acca
RoxBlox3 <RoxBlox33@proton.me>
2025-02-24 13:53:00 +0100

    Completed Ch8 and finished Ch8 C5

--- Learn_Python/Ch8_Loops/C5_Mediate/main.py
+++ Learn_Python/Ch8_Loops/C5_Mediate/main.py
@@ -1,3 +1,11 @@
 def meditate(mana, max_mana, energy, energy_drinks):
-    pass
-
+    while mana != max_mana and (energy_drinks > 0 or energy > 0):
+        if energy == 0 and energy_drinks > 0:
+            energy_drinks -= 1
+            energy += 50
+            print(energy)
+        else:
+            mana += 1
+            energy -= 1
+        print(f"{mana} and energy {energy}")
+    return mana, energy, energy_drinks
