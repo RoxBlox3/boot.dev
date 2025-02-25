987a69ed27303c6a6e3dacab682c3a58a65c8502
RoxBlox3 <RoxBlox33@proton.me>
2025-02-25 14:31:59 +0100

    Ch9 Lists finished L1-L9

---
+++ Learn_Python/Ch9_Lists/L6_List_Length/main.py
@@ -0,0 +1,3 @@
+def get_last_index(inventory):
+    last_item = len(inventory)
+    return last_item - 1
---
+++ Learn_Python/Ch9_Lists/L7_List_Updates/main.py
@@ -0,0 +1,4 @@
+def smelt_ore(inventory):
+    if inventory[1] == "Iron Ore":
+        inventory[1] = "Iron Bar"
+    return inventory
