b8913c543dbddc4dba744b4e9ea52c51a0c8d019
RoxBlox3 <RoxBlox33@proton.me>
2025-03-07 14:45:26 +0100

    Started the first loop

--- Learn_Python/Ch9_Lists/L24_Filter_Messages/main.py
+++ Learn_Python/Ch9_Lists/L24_Filter_Messages/main.py
@@ -1,2 +1,15 @@
 def filter_messages(messages):
-    pass
+    #Initialize variables
+    filtered_messages = []
+    filter_count = []
+    # For each message in the input list:
+    for message in messages:
+    # Split the message into a list of words using the .split() string method (see below for help).
+    # Create a new empty list for all the non-bad words for this message.
+    # Create a counter variable and set it to 0. We'll increment this when we remove words from this message.
+    # For each word in the message:
+    #     If the word is dang, increment the counter
+    #     If it is not dang, add the word to the non-bad word list you created
+    # Join the list of non-bad words into a single string using the .join() method (see below for help)
+    # Append the new clean message to the final list of filtered messages
+    # Append the count of bad words removed to its list
