3442961156d720a336ef3c2cf0b07fd6979ada18
RoxBlox3 <roxblox33@proton.me>
2025-03-09 20:20:16 +0100

    added a test script called remboursement

---
+++ Learn_Python/python_test/remboursements.py
@@ -0,0 +1,9 @@
+coop = 5.4 + 3.6 + 18.4
+epsic = 13 * 18
+mcdo = 13.90 + 12.40 + 15.50
+total = coop + epsic + mcdo
+print(f"Coop : {coop}")
+print(f"Epsic : {epsic}")
+print(f"McDo : {mcdo}")
+print("---------------------------")
+print(f"Total : {total}")
