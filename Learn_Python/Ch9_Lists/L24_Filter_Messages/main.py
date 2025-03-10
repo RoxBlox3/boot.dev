0be4267351e36e5f4d8618dbf4fc1b1e0c9a113a
RoxBlox3 <RoxBlox33@proton.me>
2025-03-10 13:29:17 +0100

    Finished Ch9 L24

--- Learn_Python/Ch9_Lists/L24_Filter_Messages/main.py
+++ Learn_Python/Ch9_Lists/L24_Filter_Messages/main.py
@@ -1,15 +1,27 @@
 def filter_messages(messages):
-    #Initialize variables
+    # Initialize variables
     filtered_messages = []
     filter_count = []
     # For each message in the input list:
     for message in messages:
-    # Split the message into a list of words using the .split() string method (see below for help).
-    # Create a new empty list for all the non-bad words for this message.
-    # Create a counter variable and set it to 0. We'll increment this when we remove words from this message.
-    # For each word in the message:
-    #     If the word is dang, increment the counter
-    #     If it is not dang, add the word to the non-bad word list you created
-    # Join the list of non-bad words into a single string using the .join() method (see below for help)
-    # Append the new clean message to the final list of filtered messages
-    # Append the count of bad words removed to its list
+        # Split the message into a list of words using the .split() string method (see below for help).
+        words = message.split()
+        # Create a new empty list for all the non-bad words for this message.
+        good_words = []
+        # Create a counter variable and set it to 0. We'll increment this when we remove words from this message.
+        counter = 0
+        # For each word in the message:
+        for word in words:
+            # If the word is dang, increment the counter
+            if word == "dang":
+                counter += 1
+            # If it is not dang, add the word to the non-bad word list you created
+            else:
+                good_words.append(word)
+        # Join the list of non-bad words into a single string using the .join() method (see below for help)
+        sentence = " ".join(good_words)
+        # Append the new clean message to the final list of filtered messages
+        filtered_messages.append(sentence)
+        # Append the count of bad words removed to its list
+        filter_count.append(counter)
+    return filtered_messages, filter_count
