de7cdde20df7dc82b4f78672b1d721c102cabaec
RoxBlox3 <RoxBlox33@proton.me>
2025-03-12 14:56:17 +0100

    Continued Ch9 C3

--- Learn_Python/Ch9_Lists/C3_Alchemy_Ingredients/main.py
+++ Learn_Python/Ch9_Lists/C3_Alchemy_Ingredients/main.py
@@ -1,5 +1,7 @@
 def check_ingredient_match(recipe, ingredients):
-    for i in recipe:
-        if recipe == ingredients:
-            print(recipe)
-    return recipe, ingredients
+    missing = []
+    for i in range(0, len(recipe)):
+        if recipe[i] == ingredients[i]:
+            missing.append(ingredients[i])
+    percentage = 100 * len(ingredients) / len(recipe)
+    return percentage, missing
