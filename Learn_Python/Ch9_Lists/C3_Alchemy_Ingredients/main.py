589e0e663f992536a21758060364d52d52ebf7db
RoxBlox3 <roxblox33@proton.me>
2025-03-14 20:27:51 +0100

    Can't seem to make it i'm too tired

--- Learn_Python/Ch9_Lists/C3_Alchemy_Ingredients/main.py
+++ Learn_Python/Ch9_Lists/C3_Alchemy_Ingredients/main.py
@@ -1,7 +1,10 @@
 def check_ingredient_match(recipe, ingredients):
     missing = []
     for i in range(0, len(recipe)):
-        if recipe[i] != ingredients[i]:
-            missing.append(recipe[i])
+        value = 0
+        for ingredient in range(0, len(ingredients)):
+            if recipe[i] != ingredients[ingredient] and value == 0:
+                value = 1
+                missing.append(recipe[i])
     percentage = 100 * len(missing) / len(recipe)
     return percentage, missing
