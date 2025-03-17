4105edda9bdc917d98bf67df204276a1a9fdf343
RoxBlox3 <RoxBlox33@proton.me>
2025-03-17 13:29:56 +0100

    Finished Ch9 C4

--- Learn_Python/Ch9_Lists/C4_Validate_Path/main.py
+++ Learn_Python/Ch9_Lists/C4_Validate_Path/main.py
@@ -1,2 +1,8 @@
 def validate_path(expected_path, character_path):
-    pass
+    character_name = character_path.pop(0)
+    right = 0
+    for i in range(0, len(expected_path)):
+        if expected_path[i] == character_path[i]:
+            right += 1
+    percentage = right * 100 / len(expected_path)
+    return (character_name, percentage)
