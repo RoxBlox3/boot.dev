ddc5dbc747308333d7931659638b05792e67b198
RoxBlox3 <roxblox33@proton.me>
2025-03-23 17:58:58 +0100

    Finished Ch10 L8

--- Learn_Python/Ch10_Dictionaries/L8_Iterating_Over_a_Disctionary_in_Python/main.py
+++ Learn_Python/Ch10_Dictionaries/L8_Iterating_Over_a_Disctionary_in_Python/main.py
@@ -1,6 +1,8 @@
 def get_most_common_enemy(enemies_dict):
-    highest = 0
+    highest = float("-inf")
+    enemy_name = None
     for name in enemies_dict:
         if enemies_dict[name] > highest:
             highest = enemies_dict[name]
-    return highest
+            enemy_name = name
+    return enemy_name
