06385632bc821729c70391f4bfaa03c837127e38
RoxBlox3 <RoxBlox33@proton.me>
2025-03-26 15:00:17 +0100

    Could not finish C2 started C3

--- Learn_Python/Ch10_Dictionaries/C2_Total_Score/main.py
+++ Learn_Python/Ch10_Dictionaries/C2_Total_Score/main.py
@@ -1,7 +1,10 @@
 def merge(dict1, dict2):
-    merged_dict = merge(dict1, dict2)
+    dict1[1] = 1
     return merged_dict
 
 
 def total_score(score_dict):
     pass
+
+
+# Can't currently do this one as i can't seem to figure it out rn
